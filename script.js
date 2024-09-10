87a23ffc0f3745154ba641fda1af097c972cfa3b
Grace <gracesaji111@gmail.com>
2024-09-10 12:33:08 +0530

    added 3 files

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var myCarousel = new bootstrap.Carousel(document.getElementById('imageCarousel'), {
+        interval: 3000,
+        wrap: true
+    });
+});
